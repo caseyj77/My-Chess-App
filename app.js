2f9b151d88ba837798c86867110b53eaf81c7af6
Zorishimo <caseyj77@gmail.com>
2024-07-31 16:53:41 +0000

    setting up files and dependencies

---
+++ app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.listen(3000);
